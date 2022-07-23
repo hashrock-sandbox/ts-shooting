4ca68cbc4e04af4c3a06928a22fb40a653b5fc92
hashrock <hashrock@users.noreply.github.com>
2022-07-23 18:32:59 +0900

    upf

--- main.ts
+++ main.ts
@@ -1,18 +1,135 @@
-import { EventType, WindowBuilder } from "https://deno.land/x/sdl2/mod.ts";
+import { EventType, Rect, Surface, WindowBuilder } from "https://deno.land/x/sdl2@0.5.1/mod.ts";
+import { FPS } from "https://deno.land/x/sdl2@0.5.1/examples/utils.ts";
+import { drawMap, Sprite } from "./util.ts";
 
-const window = new WindowBuilder("Hello, Deno!", 640, 480).build();
-const canvas = window.canvas();
+const canvasSize = { width: 400, height: 400 };
+const window = new WindowBuilder(
+  "Hello, Deno!",
+  canvasSize.width,
+  canvasSize.height,
+).build();
+const canv = window.canvas();
+
+const surface = Surface.fromFile("./assets/sprite-stg.png");
+
+const creator = canv.textureCreator();
+const texture = creator.createTextureFromSurface(surface);
+const tick = FPS();
+
+const map = [
+  [8, 8, 9, 8, 11, 8, 8, 8],
+  [8, 8, 8, 8, 8, 8, 8, 8],
+  [8, 10, 8, 8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8, 8, 10, 8],
+  [8, 8, 8, 8, 8, 9, 8, 8],
+  [10, 8, 8, 8, 8, 8, 8, 8],
+  [8, 8, 11, 8, 8, 8, 8, 8],
+];
+
+const denoTextureFrames = [
+  new Rect(0, 0, 16, 16),
+  new Rect(16, 0, 16, 16),
+  new Rect(32, 0, 16, 16),
+  new Rect(48, 0, 16, 16),
+];
+
+const shadowTexture = [
+  new Rect(0, 3 * 16, 16, 16),
+];
+
+function random(min: number, max: number) {
+  return (Math.random() * (max - min) + min) | 0;
+}
+
+function createShadowInstance() {
+  const shadow = new Sprite(texture, shadowTexture);
+  shadow.x = 0;
+  shadow.y = 0;
+  shadow.originX = shadow.frames[0].width / 2 + 6;
+  shadow.originY = shadow.frames[0].height - 16;
+  shadow.scale = 4;
+  shadow.vx = 0;
+  shadow.vy = 0;
+  return shadow;
+}
+
+function createDenoInstance() {
+  const deno = new Sprite(texture, denoTextureFrames);
+  deno.x = random(0, canvasSize.width);
+  deno.y = random(0, canvasSize.height);
+  deno.originX = deno.frames[0].width / 2;
+  deno.originY = deno.frames[0].height;
+  deno.scale = 4;
+  deno.vx = 0.02;
+  deno.vy = 0.01;
+  return deno;
+}
+
+const denos: Sprite[] = [];
+
+for (let i = 0; i < 1; i++) {
+  denos.push(createDenoInstance());
+}
+
+const shadow = createShadowInstance();
+
+let cnt = 0;
+
+function frame() {
+  canv.clear();
+  drawMap(texture, canv, map, 16);
+
+  for (const deno of denos) {
+    deno.tick();
+    shadow.draw(canv);
+    deno.draw(canv);
+
+    const margin = 48;
+    deno.wrap({
+      x: -margin,
+      y: -margin,
+      width: canvasSize.width + margin * 2,
+      height: canvasSize.height + margin * 2,
+    });
+
+    shadow.x = deno.x;
+    shadow.y = deno.y;
+
+    // make deno jump
+    deno.z = Math.abs(Math.sin(cnt / 10) * 16) | 0;
+
+    if ((cnt / 20 | 0) % 2 === 0) {
+      if (deno.vx > 0) {
+        deno.index = 2;
+      } else {
+        deno.index = 0;
+      }
+    } else {
+      if (deno.vx > 0) {
+        deno.index = 3;
+      } else {
+        deno.index = 1;
+      }
+    }
+
+    cnt++;
+  }
+
+  canv.present();
+  tick()
+  // Deno.sleepSync(10);
+}
 
 for (const event of window.events()) {
-  if (event.type == EventType.Quit) {
-    break;
-  } else if (event.type == EventType.Draw) {
-    // Rainbow effect
-    const r = Math.sin(Date.now() / 1000) * 127 + 128;
-    const g = Math.sin(Date.now() / 1000 + 2) * 127 + 128;
-    const b = Math.sin(Date.now() / 1000 + 4) * 127 + 128;
-    canvas.setDrawColor(Math.floor(r), Math.floor(g), Math.floor(b), 255);
-    canvas.clear();
-    canvas.present();
+  switch (event.type) {
+    case EventType.Draw:
+      frame();
+      break;
+    case EventType.Quit:
+      Deno.exit(0);
+      break;
+    default:
+      break;
   }
 }
---
+++ util.ts
@@ -0,0 +1,77 @@
+import { Canvas, Rect, Texture } from "https://deno.land/x/sdl2@0.5.1/mod.ts";
+
+export interface Area {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export function drawMap(
+  texture: Texture,
+  canvas: Canvas,
+  map: number[][],
+  chipSize: number,
+) {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      const chip = map[i][j];
+      const src = new Rect(
+        (chip % 4) * chipSize,
+        ((chip / 4) | 0) * chipSize,
+        chipSize,
+        chipSize,
+      );
+      const dst = new Rect(
+        i * chipSize * 4,
+        j * chipSize * 4,
+        chipSize * 4,
+        chipSize * 4,
+      );
+      canvas.copy(
+        texture,
+        src,
+        dst,
+      );
+    }
+  }
+}
+
+export class Sprite {
+  x = 0;
+  y = 0;
+  z = 0;
+  vx = 0;
+  vy = 0;
+  originX = 0;
+  originY = 0;
+  scale = 1;
+  texture: Texture;
+  frames: Rect[];
+  index = 0;
+
+  constructor(texture: Texture, frames: Rect[]) {
+    this.texture = texture;
+    this.frames = frames;
+  }
+
+  draw(dest: Canvas) {
+    const dst = new Rect(
+      this.x - this.originX,
+      this.y - this.originY - this.z,
+      this.frames[this.index].width * this.scale,
+      this.frames[this.index].height * this.scale,
+    );
+    dest.copy(this.texture, this.frames[this.index], dst);
+  }
+
+  tick() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  wrap(rect: Area) {
+    this.x = (this.x - rect.x) % rect.width + rect.x;
+    this.y = (this.y - rect.y) % rect.height + rect.y;
+  }
+}
