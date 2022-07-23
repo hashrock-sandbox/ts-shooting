b33d064da5e08d24e234d70244a41b9137703671
hashrock <hashrock@users.noreply.github.com>
2022-07-23 19:54:41 +0900

    upd

--- main.ts
+++ main.ts
@@ -40,7 +40,22 @@ function createDenoInstance() {
 const deno = createDenoInstance();
 
 let cnt = 0;
-const speed = 0.05
+const speed = 0.1
+
+class KeyboardStack {
+  _keys: Set<number> = new Set();
+  isDown(keycode: number) {
+    return this._keys.has(keycode);
+  }
+
+  down(keycode: number){
+    this._keys.add(keycode);
+  }
+  up(keycode: number){
+    this._keys.delete(keycode);
+  }
+}
+
 
 function frame() {
   canv.clear();
@@ -51,38 +66,59 @@ function frame() {
   tick()
 }
 
-let keyUp = false;
-let keyDown = false;
-let keyLeft = false;
-let keyRight = false;
-
+let keyUp = false; //
+let keyDown = false; //
+let keyLeft = false; //
+let keyRight = false; //
 
 for (const event of window.events()) {
-  console.log(event.type)
   switch (event.type) {
     case EventType.Draw:
       frame();
-
-
-
-      
+      if(keyUp){
+        deno.y -= speed;
+      }
+      if(keyDown){
+        deno.y += speed;
+      }
+      if(keyLeft){
+        deno.x -= speed;
+      }
+      if(keyRight){
+        deno.x += speed;
+      }
       break;
     case EventType.Quit:
       Deno.exit(0);
       break;
+    case EventType.KeyUp:
+      const keycode2 = event.keysym.scancode
+      if (keycode2 === 80) {
+        keyLeft = false;
+      }
+      if (keycode2 === 82) {
+        keyUp = false
+      }
+      if (keycode2 === 79) {
+        keyRight = false
+      }
+      if (keycode2 === 81) {
+        keyDown = false
+      }
+      break;
     case EventType.KeyDown:
       const keycode = event.keysym.scancode
       if (keycode === 80) {
-        deno.x -= speed;
+        keyLeft = true;
       }
       if (keycode === 82) {
-        deno.y -= speed;
+        keyUp = true
       }
       if (keycode === 79) {
-        deno.x += speed;
+        keyRight = true
       }
       if (keycode === 81) {
-        deno.y += speed;
+        keyDown = true
       }
       break;
     default:
